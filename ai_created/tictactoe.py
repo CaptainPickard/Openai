9fe52d2694f5b98cb41fc6392fbef0e5ef5d2275
CaptainPickard <pickard.nicko@gmail.com>
2022-10-21 17:46:36 -0600

    Update tictactoe.py

--- ai_created/tictactoe.py
+++ ai_created/tictactoe.py
@@ -16,9 +16,6 @@
 #         print("It's a draw!")
 #         break
 
-
-
-
 # Player 1:
 
 # X
@@ -28,43 +25,31 @@
 # O
 
 
+# def print_board(board):
+#     print("The board look like this: \n")
 
-# {
+#     for i in range(3):
+#         print(" ", end=" ")
+#         for j in range(3):
+#             if board[i*3+j] == 1:
+#                 print("X ", end=" ")
+#             elif board[i*3+j] == 0:
+#                 print("O ", end=" ")
+#             elif board[i*3+j] != -1:
+#                 print(board[i*3+j]-1, end=" ")
+#             else:
+#                 print(" ", end=" ")
 
-# (1,1): " ", (1,2): " ", (1,3): " ",
-# (2,1): " ", (2,2): " ", (2,3): " ",
-# (3,1): " ", (3,2): " ", (3,3): " "
-# }
+#         print()
 
+# def print_instruction():
+#     print("Please use the following cell numbers to make your move")
+#     print_board([2,3,4,5,6,7,8,9,10])
 
-
-
-
-def print_board(board):
-    print("The board look like this: \n")
-
-    for i in range(3):
-        print(" ", end=" ")
-        for j in range(3):
-            if board[i*3+j] == 1:
-                print("X ", end=" ")
-            elif board[i*3+j] == 0:
-                print("O ", end=" ")
-            elif board[i*3+j] != -1:
-                print(board[i*3+j]-1, end=" ")
-            else:
-                print(" ", end=" ")
-
-        print()
-
-def print_instruction():
-    print("Please use the following cell numbers to make your move")
-    print_board([2,3,4,5,6,7,8,9,10])
-
-def get_input(turn):
-    valid = False
-    while not valid:
-        try:
-            player = input("Where would you like to place " + turn + " (1-9)? ")
-            player = int(player)
-            if player
+# def get_input(turn):
+#     valid = False
+#     while not valid:
+#         try:
+#             player = input("Where would you like to place " + turn + " (1-9)? ")
+#             player = int(player)
+#             if player
